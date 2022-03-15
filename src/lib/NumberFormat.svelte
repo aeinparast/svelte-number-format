d720907b1bcee577d0bb6970cf487df251990f34
Pitis <pitis.radu@gmail.com>
2022-03-15 22:35:21 +0200

    fix: revert routes

--- src/lib/NumberFormat.svelte
+++ src/lib/NumberFormat.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
   export let placeholder = ''
+  export let displayType = 'input'
+  export let value: string | number = null
+  export let type = 'text'
+  export let prefix = ''
+
+  let formattedValue = `${prefix} ${value}`
+
+  function getCurrentCaretPosition(el: HTMLInputElement) {
+    /*Max of selectionStart and selectionEnd is taken for the patch of pixel and other mobile device caret bug*/
+    return Math.max(el.selectionStart, el.selectionEnd)
+  }
 </script>
 
-<input {placeholder} />
+{#if displayType === 'input'}
+  <input
+    {type}
+    {placeholder}
+    value={formattedValue}
+    on:keydown={(e) => {
+      console.log(getCurrentCaretPosition(e.target))
+    }}
+  />
+{/if}
+
+{#if displayType === 'text'}
+  <span>{value}</span>
+{/if}
