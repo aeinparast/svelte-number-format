952aaa07655972c9a82de296e58491e1965aa86d
Pitis <pitis.radu@protonmail.com>
2024-11-17 04:17:27 +0200

    feat: new update

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import NumberFormat from '../lib/NumberFormat.svelte'
+
+	let formattedValue = ''
+	let rawValue = ''
+</script>
+
+<NumberFormat
+	value={1234567.89}
+	format="#,###.##"
+	decimalSeparator="."
+	thousandSeparator=","
+	onInput={(formatted, raw) => {
+		formattedValue = formatted
+		rawValue = raw
+	}}
+/>
+
+<p>Formatted: {formattedValue}</p>
+<p>Raw: {rawValue}</p>
